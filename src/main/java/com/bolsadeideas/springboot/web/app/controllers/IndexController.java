8b61d5b474f24ed5aac6d8416c85862789fc6140
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-01 20:57:37 +0200

    perfilesAnadidos

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,7 +1,9 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,26 @@ public class IndexController {
 
     @GetMapping({"/index","/","/home",""})
     public String index(ModelMap model){
-        model.addAttribute("titulo","holaSpring framework");
+
+        //variable th titulo
+        model.addAttribute("titulo","hola Spring framework");
         return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil (Model model){
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Andrés");
+        usuario.setApellido("Guzman");
+
+
+        model.addAttribute("titulo","Perfil del usuario: ".concat(usuario.getNombre()));
+        //variables que luego usamos en el th(usuario comillas" y luego la instancia del usuario
+        model.addAttribute("usuario",usuario);
+
+
+        return "perfil";
     }
 
 }
