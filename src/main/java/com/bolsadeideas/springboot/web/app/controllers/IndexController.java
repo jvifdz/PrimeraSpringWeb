4df61e204c6fcd6dd7099265d579fb5329a0d1cc
jvifdzbjj-GRANDE <javiason@gmail.com>
2020-10-03 22:51:36 +0200

    añadidas variables con la anotacion @value

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.bolsadeideas.springboot.web.app.controllers;
 
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -16,11 +17,23 @@ import java.util.List;
 @RequestMapping("/app")
 public class IndexController {
 
+    @Value("${texto.indexcontroller.index.titulo}")
+    private String textoIndex;
+
+    @Value("${texto.indexcontroller.perfil.titulo}")
+    private String textoPerfil;
+
+    @Value("${texto.indexcontroller.listar.titulo}")
+    private String textoListar;
+
+
     @GetMapping({"/index", "/", "/home", ""})
     public String index(ModelMap model) {
 
         //variable th titulo
-        model.addAttribute("titulo", "hola Spring framework");
+        //model.addAttribute("titulo", "hola Spring framework");
+        //manera con el value
+        model.addAttribute("titulo", textoIndex);
         return "index";
     }
 
@@ -32,8 +45,10 @@ public class IndexController {
         usuario.setApellido("Guzman");
         usuario.setEmail("pepe@gmail.com");
 
+            //manera con el value
+            /* model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));*/
+            model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 
-        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
         //variables que luego usamos en el th(usuario comillas" y luego la instancia del usuario
         model.addAttribute("usuario", usuario);
 
@@ -44,8 +59,9 @@ public class IndexController {
     @RequestMapping("/listar")
     public String listar(Model model) {
 
-        model.addAttribute("titulo", "Lista de usuarios: ");
-
+        //model.addAttribute("titulo", "Lista de usuarios: ");
+        //manera con el value
+        model.addAttribute("titulo", textoListar);
         //Uso el model Atrribute para generalizar el añadido a la lista
         /*List<Usuario> usuarios = new ArrayList<>();
 
